e71da1f7148f0797fdea1e888815e0e2ed867112
Prashant Sharma <prashantsharma7470@gmail.com>
2024-07-27 22:42:26 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,28 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <fieldset>
+        <label style={{ backgroundImage: 'url(https://www.jokesinhindishayari.com/wallpapers-src/god-ram-3d-wallpaper/9bc709f2130e59029a8061a7715a0349.jpg)' }}>
+          <input type="radio" name="images" value="Jai Shree Ram" />
+        </label>
+        <label style={{ backgroundImage: 'url(https://d2jnu6hkti1tqv.cloudfront.net/upload/7055be07-5cdd-4abb-8bfb-76a4562924bd.jpg)' }}>
+          <input type="radio" name="images" value="Jai Shree Ram" />
+        </label>
+        <label style={{ backgroundImage: 'url(https://cdn.pixabay.com/photo/2024/02/08/03/45/ai-generated-8560118_640.png)' }}>
+          <input type="radio" name="images" value="Jai Shree Ram" />
+        </label>
+        <label style={{ backgroundImage: 'url(https://t4.ftcdn.net/jpg/05/54/24/09/360_F_554240992_kx5rRzhEySBQF3yAgOv0OUMGHK7FarcP.jpg)' }}>
+          <input type="radio" name="images" value="Jai Shree Ram" />
+        </label>
+        <label style={{ backgroundImage: 'url(https://img.freepik.com/premium-photo/lord-ganesh-jee_1153477-894.jpg)' }}>
+          <input type="radio" name="images" value="Jai Shree Ram" />
+        </label>
+      </fieldset>
+    </>
+  )
+}
+
+export default App
